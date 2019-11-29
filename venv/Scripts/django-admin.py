c5a731748212d017461088c70f42dc497895a56c
Damian Swieca <damianswieca@my.ccsu.edu>
2019-11-29 08:27:32 -0500

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Damian\PycharmProjects\videogame_catalog\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
